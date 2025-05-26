53ff7e9ffee6c2a0b09a83ccc4a257c642f74338
Dige945 <2471722964@qq.com>
2025-05-26 10:18:48 +0800

    update

--- src/main/java/com/example/database/config/CorsConfig.java
+++ src/main/java/com/example/database/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081")
+                .allowedOriginPatterns("http://localhost:[*]")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
